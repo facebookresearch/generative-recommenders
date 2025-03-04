a87192fdc0108576e95f373ec99725562d19d3ae
Xing Liu <xingl@meta.com>
2025-03-03 21:53:18 -0800

    Refactor MultitaskModule
    Differential Revision: D70518517

--- generative_recommenders/modules/dlrm_hstu.py
+++ generative_recommenders/modules/dlrm_hstu.py
@@ -33,6 +33,7 @@ from generative_recommenders.common import (
 from generative_recommenders.modules.hstu_transducer import HSTUTransducer
 from generative_recommenders.modules.multitask_module import (
     DefaultMultitaskModule,
+    MultitaskTaskType,
     TaskConfig,
 )
 from generative_recommenders.modules.positional_encoder import HSTUPositionalEncoder
@@ -97,6 +98,25 @@ class DlrmHSTUConfig:
     use_layer_norm_postprocessor: bool = False
 
 
+def _get_supervision_labels_and_weights(
+    supervision_bitmasks: torch.Tensor,
+    watchtime_sequence: torch.Tensor,
+    task_configs: List[TaskConfig],
+) -> Tuple[Dict[str, torch.Tensor], Dict[str, torch.Tensor]]:
+    supervision_labels: Dict[str, torch.Tensor] = {}
+    supervision_weights: Dict[str, torch.Tensor] = {}
+    for task in task_configs:
+        if task.task_type == MultitaskTaskType.REGRESSION:
+            supervision_labels[task.task_name] = watchtime_sequence
+        elif task.task_type == MultitaskTaskType.BINARY_CLASSIFICATION:
+            supervision_labels[task.task_name] = (
+                torch.bitwise_and(supervision_bitmasks, task.task_weight) > 0
+            ).to(torch.int)
+        else:
+            raise RuntimeError("Unsupported MultitaskTaskType")
+    return supervision_labels, supervision_weights
+
+
 class DlrmHSTU(HammerModule):
     def __init__(  # noqa C901
         self,
@@ -115,9 +135,8 @@ class DlrmHSTU(HammerModule):
             device=torch.device("meta"),
         )
 
-        # multitask configs, must sort by task types
+        # multitask configs must be sorted by task types
         self._multitask_configs: List[TaskConfig] = hstu_configs.multitask_configs
-        self._multitask_configs.sort(key=lambda x: x.task_type)
         self._multitask_module = DefaultMultitaskModule(
             task_configs=self._multitask_configs,
             embedding_dim=hstu_configs.hstu_transducer_embedding_dim,
@@ -126,8 +145,6 @@ class DlrmHSTU(HammerModule):
                 SwishLayerNorm(512),
                 torch.nn.Linear(in_features=512, out_features=num_tasks),
             ).apply(init_mlp_weights_optional_bias),
-            candidates_weight_feature_name=self._hstu_configs.candidates_weight_feature_name,
-            candidates_watchtime_feature_name=self._hstu_configs.candidates_watchtime_feature_name,
             causal_multitask_weights=hstu_configs.causal_multitask_weights,
             is_inference=self._is_inference,
         )
@@ -455,11 +472,23 @@ class DlrmHSTU(HammerModule):
                 num_candidates=num_candidates,
             )
         with record_function("## multitask_module ##"):
+            supervision_labels, supervision_weights = (
+                _get_supervision_labels_and_weights(
+                    supervision_bitmasks=payload_features[
+                        self._hstu_configs.candidates_weight_feature_name
+                    ],
+                    watchtime_sequence=payload_features[
+                        self._hstu_configs.candidates_watchtime_feature_name
+                    ],
+                    task_configs=self._multitask_configs,
+                )
+            )
             mt_target_preds, mt_target_labels, mt_target_weights, mt_losses = (
                 self._multitask_module(
                     encoded_user_embeddings=candidates_user_embeddings,
                     item_embeddings=candidates_item_embeddings,
-                    payload_features=payload_features,
+                    supervision_labels=supervision_labels,
+                    supervision_weights=supervision_weights,
                 )
             )
 
