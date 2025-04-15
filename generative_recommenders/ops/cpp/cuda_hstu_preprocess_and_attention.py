4b8a4eb2e7ed6906db0e2f5a20a36b7ef898fcd3
Chloe Liu <ruochenl@meta.com>
2025-04-15 14:23:15 -0700

    Back out "Revert D71788655: [HSTU] Add the option to fuse silu(u) into compute_output"
    Differential Revision: D72987077

--- generative_recommenders/ops/cpp/cuda_hstu_preprocess_and_attention.py
+++ generative_recommenders/ops/cpp/cuda_hstu_preprocess_and_attention.py
@@ -60,6 +60,7 @@ class _HSTUPreprocessAndAttentionFunction(torch.autograd.Function):
         sort_by_length: bool = False,
         max_attn_len: Optional[int] = None,
         full_attn_size: Optional[int] = None,
+        silu_u: bool = True,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         max_attn_len = max_attn_len or 0
         full_attn_size = full_attn_size or 0
@@ -83,7 +84,8 @@ class _HSTUPreprocessAndAttentionFunction(torch.autograd.Function):
         q = q.view(-1, num_heads, attn_dim)
         k = k.view(-1, num_heads, attn_dim)
         v = v.view(-1, num_heads, hidden_dim)
-        silu_u = F.silu(u)
+        if silu_u:
+            u = F.silu(u)
         out = torch.ops.hstu.hstu_mha_fwd(
             max_seq_len,
             alpha,
@@ -137,13 +139,14 @@ class _HSTUPreprocessAndAttentionFunction(torch.autograd.Function):
         ctx.norm_num_warps = num_warps
         ctx.contextual_seq_len = contextual_seq_len
         ctx.sort_by_length = sort_by_length
-        return silu_u, out
+        ctx.silu_u = silu_u
+        return u, out
 
     @staticmethod
     # pyre-ignore[14]
     def backward(
         ctx,  # pyre-ignore[2]
-        dsilu_u: torch.Tensor,
+        _du: torch.Tensor,
         dout: torch.Tensor,
     ) -> Tuple[
         torch.Tensor,  # d_x
@@ -155,6 +158,7 @@ class _HSTUPreprocessAndAttentionFunction(torch.autograd.Function):
         None,
         torch.Tensor,  # d_uvqk_weight
         torch.Tensor,  # d_uvqk_bias
+        None,
         None,
         None,
         None,
@@ -248,7 +252,11 @@ class _HSTUPreprocessAndAttentionFunction(torch.autograd.Function):
             dk.copy_(_dk)
         if dv.data_ptr() != _dv.data_ptr():
             dv.copy_(_dv)
-        torch.ops.aten.silu_backward(dsilu_u, u, grad_input=du)
+        if ctx.silu_u:
+            torch.ops.aten.silu_backward(_du, u, grad_input=du)
+        else:
+            if du.data_ptr() != _du.data_ptr():
+                du.copy_(_du)
         d_normed_x, d_uvqk_weight, d_uvqk_bias = triton_addmm_bwd(
             x=normed_x,
             w=uvqk_weight,
@@ -289,6 +297,7 @@ class _HSTUPreprocessAndAttentionFunction(torch.autograd.Function):
             None,
             None,
             None,
+            None,
         )
 
 
@@ -313,6 +322,7 @@ def cuda_hstu_preprocess_and_attention(
     sort_by_length: bool = False,
     max_attn_len: Optional[int] = None,
     full_attn_size: Optional[int] = None,
+    silu_u: bool = True,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     return _HSTUPreprocessAndAttentionFunction.apply(
         x,
@@ -335,4 +345,5 @@ def cuda_hstu_preprocess_and_attention(
         sort_by_length,
         max_attn_len,
         full_attn_size,
+        silu_u,
     )
